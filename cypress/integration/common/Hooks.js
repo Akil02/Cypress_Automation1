d7a05f60ceb2e7ead4c0326e3a9df2646ca383f1
A Akil Ganesh <akilganesh3@gmail.com>
2024-03-03 12:02:34 +0530

    Changes in hooks class

--- cypress/integration/common/Hooks.js
+++ cypress/integration/common/Hooks.js
@@ -3,10 +3,8 @@ import { Before, After } from "cypress-cucumber-preprocessor/steps";
 Before(() => {
   cy.clearCookies();
   cy.clearLocalStorage();
-  cy.log("-------------------------------------------------------");
 });
 
 After(() => {
   cy.log("Scenario Complete");
-  cy.log("-------------------------------------------------------");
 });
